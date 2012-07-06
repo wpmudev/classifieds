ba03e73e594cf4c11c2da40660bb10abb97513bc
arnold <arnold@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-07-06 17:34:58 +0000

    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/classifieds@6547 f53fe46e-8c02-459b-9fdd-93e75771b278

--- loader.php
+++ loader.php
@@ -42,6 +42,8 @@ define( 'CF_TEXT_DOMAIN', 'classifieds' );
 // The key for the options array
 define( 'CF_OPTIONS_NAME', 'classifieds_options' );
 
+// include core files
+include_once 'core/wpmudev-dash-notification.php';
 //If another version of CustomPress not loaded, load ours.
 if(!class_exists('CustomPress_Core')) include_once 'custompress/loader.php';
 
@@ -49,14 +51,3 @@ if(!class_exists('CustomPress_Core')) include_once 'custompress/loader.php';
 include_once 'core/core.php';
 include_once 'core/paypal.php';
 include_once 'core/functions.php';
-
-
-/* Update Notifications Notice */
-if ( !function_exists( 'wdp_un_check' ) ) {
-	add_action( 'admin_notices', 'wdp_un_check', 5 );
-	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-	function wdp_un_check() {
-		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'install_plugins' ) )
-		echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-	}
-}
